aaaa2e1b95a301825cd11d04c25021ff7dc8f217
Matthew Alexander LaChance <mattlach@umich.edu>
2018-09-11 22:29:11 -0400

    :white_check_mark: Add the bare role

---
+++ manifests/role/bare.pp
@@ -0,0 +1,26 @@
+# hemphill: My puppet control module
+# Copyright (C) 2018 Matt LaChance
+#
+# Hemphill is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Hemphill is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with hemphill. If not, see <http://www.gnu.org/licenses/>.
+
+# hemphill::role::bare
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include hemphill::role::bare
+class hemphill::role::bare {
+}
---
+++ spec/classes/role/bare_spec.rb
@@ -0,0 +1,26 @@
+# hemphill: My puppet control module
+# Copyright (C) 2018 Matt LaChance
+#
+# Hemphill is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Hemphill is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with hemphill. If not, see <http://www.gnu.org/licenses/>.
+require 'spec_helper'
+
+describe 'hemphill::role::bare' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
